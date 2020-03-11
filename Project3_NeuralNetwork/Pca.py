fdcfdc274dde043b4db52d14da6e2a567db7db50
zengdewei1 <2313970511@qq.com>
2020-03-11 21:26:15 +0800

    修复pca

--- Project3_NeuralNetwork/Pca.py
+++ Project3_NeuralNetwork/Pca.py
@@ -1,6 +1,6 @@
 from sklearn.decomposition import PCA
 
-def pca_process(n_components):
+def pca_process(X, n_components):
     # n_components 选择要下降到多少维度
-    X = PCA(n_components=2).fit_transform(X)
+    X = PCA(n_components=n_components).fit_transform(X)
     return X
